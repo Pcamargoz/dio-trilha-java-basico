fe4b56a2eedac9232caac127df455e1b4c8ddd05
Pcamargoz <pedro01cesarsanto@gmail.com>
2025-03-16 03:02:30 -0300

    Atualizações de ilustrações

---
+++ java-anatomia-classes/src/edu/pedro/primeirasemana/Primeiros Passos/Calculadora.java
@@ -0,0 +1,25 @@
+package edu.pedro.primeirasemana.Primeiros Passos;
+    /** 
+     * 
+     * @author Pedro Camargo
+     * @version 1.0
+     * @since 16/03/2025
+     */
+    
+public class Calculadora {
+    /** 
+    * este método é utilizado para somar dois números inteiros
+    * @param numeroUm este é o primeiro parâmetro do método
+    * @param numeroDois este é o segundo parâmetro do método
+    * @return int o resultado deste método é a soma dos dois números.
+    */
+    public int somar(int numeroUm, int numeroDois) {
+        return numeroUm + numeroDois;
+    }
+
+        
+    }
+    
+        
+    
+
---
+++ java-anatomia-classes/src/edu/pedro/primeirasemana/Primeiros Passos/Comentarios.java
@@ -0,0 +1,27 @@
+package edu.pedro.primeirasemana.Primeiros Passos;
+
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentario em uma única linha
+
+        /* Olá,
+         * eu sou um comentario 
+         * que posso ser mais detalhado
+         * quando nescessário
+         * 
+         */
+
+    }
+    /**
+     * Estas duas estrelinhas acima 
+     * é para indentificar que você
+     * pretende elaborar um comentário
+     * a nivel de documentação
+     * Que incrivel !!!!
+    */
+    public void metodo(){
+
+    }
+
+
+}
